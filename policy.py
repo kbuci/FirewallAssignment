7692985b922c6624a99799ce05474719cbcd4f06
Krishen Bhan <krishenb@uci.edu>
2019-11-05 20:07:52 -0800

    Update policy.py

--- policy.py
+++ policy.py
@@ -6,7 +6,7 @@ AllowedRange = namedtuple('AllowedRange','lower higher id')
 # with the corresponding policy ids (row # in csv) for each range.
 # Upon querying a target value, PolicyRange iterates through
 # all of the ranges the target is in, and returns the corresponding
-# policy id for each.
+# policy id for each. The updated flag is used to check if the self.ranges needs to be sorted (upon calling find_all)
 class PolicyRange:
     def __init__(self):
         self.ranges = []
@@ -16,7 +16,7 @@ class PolicyRange:
         return self.ranges.__repr__()
         
 
-    def insert_range(self, lower, higher, policy_id):
+    def insert_range(self, lower, higher, policy_id): # Insert a new range just by appending it to the end of the list
         self.ranges.append(AllowedRange(lower,higher,policy_id))
         self.updated = False
     
@@ -24,7 +24,7 @@ class PolicyRange:
         self.ranges.sort(key=lambda r: (r.lower, r.higher))
         self.updated = True
     
-    def _find_value_range(self, target):
+    def _find_value_range(self, target): #Do a variation of a binary search to find the left most index in self.ranges that contains target
         left, right = 0, len(self.ranges) - 1
         while left < right:
             mid = left + (right - left)//2
@@ -43,10 +43,10 @@ class PolicyRange:
 
 
     def find_all(self, target):
-        if not self.updated:
+        if not self.updated: #sort self.ranges if there have been any update since the last time find_all was called
             self._update()
 
-        left = self._find_value_range(target)
+        left = self._find_value_range(target) #get the range with the smallest start value that has the target, and iterate right until you get to a range that is greater than target
         found_ids = set()
         for allowed in self.ranges[left:]:
             if allowed.lower <= target <= allowed.higher:
