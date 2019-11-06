8b77e1830369d8f60ac93e5737b30f3e43a12615
Krishen Bhan <krishenb@uci.edu>
2019-11-05 19:56:56 -0800

    Update run_firewall.py

--- run_firewall.py
+++ run_firewall.py
@@ -2,8 +2,6 @@ from firewall import Firewall
 
 if __name__ == "__main__":
     fw = Firewall("prompt_input.csv")
-    print(fw.port_policies)
-    print(fw.ip_policies)
     assert(fw.accept_packet("inbound", "tcp", 80, "192.168.1.2")) # matches first rule
     assert(fw.accept_packet("inbound", "udp", 53, "192.168.2.1")) # matches third rule
     assert(fw.accept_packet("outbound", "tcp", 10234, "192.168.10.11")) # matches second rule true
